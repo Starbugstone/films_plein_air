e0af8f27f5112287e2547e48b5400280a62e169b
StarbugStone <pint2stone@gmail.com>
2018-04-06 00:03:27 +0200

    fix #3
    added the colors to variables

--- app/index.php
+++ app/index.php
@@ -8,11 +8,13 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <link rel="css/style.css" />
+
+    <title>Les films de plein air</title>
   </head>
   <body>
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-  
+
       <a class="navbar-brand" href="#">Expand at md</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -39,9 +41,9 @@
           </li>
         </ul>
       </div>
-	  
+
     </nav>
-  
+
 	<?php
 	echo("Test me twice");
 	?>
@@ -52,11 +54,11 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-	
+
 	<!-- Vue.js and Carousel-vue-3d -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.16/vue.js"></script>
 	<script src="vendor/vue-carousel-3d/vue-carousel-3d.min.js"></script>
-	
+
 	<!-- Local Script -->
 	<script src="js/script.js"></script>
   </body>
