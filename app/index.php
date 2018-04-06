f506196593a052dcb63739b0b127e9251efffe80
StarbugStone <pint2stone@gmail.com>
2018-04-06 10:18:34 +0200

    adding the footer
    integrating the footer

--- app/index.php
+++ app/index.php
@@ -114,7 +114,28 @@
       </div>
     </main>
     <footer>
+      <div class="row justify-content-around footerRow">
+        <?php
+        for ($i=0; $i < 3; $i++) {
+          ?>
+          <div class="col-md-4">
+            <div class="card">
+              <div class="card-header">
+                Featured
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <?php
+        }
+        ?>
 
+
+      </div><!-- End Footer Row -->
 
     </footer>
 
